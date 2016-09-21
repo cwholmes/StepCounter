a3150e9d59a58c960d8febf90c141868aa814c39
Raquib Talukder <raquib-talukder@uiowa.edu>
2016-09-20 21:12:02 -0500

    Update mainActivity.java

--- mainActivity.java
+++ mainActivity.java
@@ -13,6 +13,8 @@ import android.widget.Button;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ArrayBlockingQueue;
 import android.widget.Toast;
+import java.io.FileOutputStream;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener ,
         OnClickListener {
@@ -28,9 +30,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         _sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorDataBuffer = new ArrayBlockingQueue<putData>(30);
+        sharedQueuePC = new LinkedBlockingQueue<putData>(30);
 
         accel = _sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        
         gyro = _sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        
         btnStart = (Button) findViewById(R.id.btnStart);
         btnStop = (Button) findViewById(R.id.btnStop);
         btnStart.setOnClickListener(this);
@@ -44,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onResume();
         _sensorManager.registerListener(this, accel,
                 SensorManager.SENSOR_DELAY_FASTEST);
+                
         _sensorManager.registerListener(this, gyro,
                 SensorManager.SENSOR_DELAY_FASTEST);
     }
@@ -63,12 +70,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (started){
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
             double x = event.values[0];
             double y = event.values[1];
             double z = event.values[2];
             long timestamp = event.timestamp;
+            
             data = new putData(timestamp, "A", x, y, z);
+            sensorDataBuffer.offer(data);
         }
 
         if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
@@ -76,7 +85,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             double y = event.values[1];
             double z = event.values[2];
             long timestamp = event.timestamp;
+            
             data = new putData(timestamp, "G", x, y, z);
+            sensorDataBuffer.offer(data);
         }
     }
 
@@ -87,6 +98,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 btnStart.setEnabled(false);
                 btnStop.setEnabled(true);
                 started = true;
+                
                 _sensorManager.registerListener(this, accel,
                         SensorManager.SENSOR_DELAY_FASTEST);
                 _sensorManager.registerListener(this, gyro,
@@ -103,11 +115,88 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
 
     }
+    
+    public class ProducerConsumerPattern {
+
+        //Creating Producer and Consumer Thread
+        Thread prodThread = new Thread(new Producer(sensorDataBuffer));
+        Thread consThread = new Thread(new Consumer(sharedQueuePC));
+
+        /*Starting producer and Consumer thread
+        prodThread.start()
+        consThread.start();
+        */
+    }
+
+    public class Producer implements Runnable {
+        private BlockingQueue<putData> sharedQueue;
+
+        public Producer(BlockingQueue sharedQueue) {
+            this.sharedQueue = sharedQueue;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    sharedQueuePC.put(sharedQueue.take());
+                }
+                catch (InterruptedException ex) {
+
+                }
+            }
+        }
+
+    }
+
+    class Consumer implements Runnable {
+        private BlockingQueue<putData> writingQueue;
+        private String fileName = "outputFile";
+
+        public Consumer(BlockingQueue<putData> sharedQueue) {
+            writingQueue = sharedQueue;
+        }
+
+        public void run() {
+            try {
+                while (true) {
+                    consume(writingQueue.take());
+                }
+            }
+            catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        void consume(Object queueItem) {
+            String content = queueItem.toString();
+
+            FileOutputStream outputStream = null;
+            try {
+                outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+                outputStream.write(content.getBytes());
+            }
+
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 
     /** Called when the user clicks the Send button */
    /** public void startWalk(View view) {
         Intent intent = new Intent(this, sensorRun.class);
         startActivity(intent);
     }**/
+    
+    /* Creating a directory 
+
+    File mydir = context.getDir("mydir", Context.MODE_PRIVATE); //Creating an internal dir;
+    File fileWithinMyDir = new File(mydir, "myfile"); //Getting a file within the dir.
+    FileOutputStream out = new FileOutputStream(fileWithinMyDir);
+
+
+     */
 }
 
